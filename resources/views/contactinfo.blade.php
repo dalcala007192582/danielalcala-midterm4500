ce4cd5ec15f1cd46944a608536b77bd630bbda0d
da2814702 <51937975+da2814702@users.noreply.github.com>
2022-03-30 21:10:10 -0700

    update

---
+++ resources/views/contactinfo.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+
+@section('title', 'title')
+
+@section('content_header')
+    <h1>Header</h1>
+@stop
+
+@section('content')
+    <p>Content here</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@stop
